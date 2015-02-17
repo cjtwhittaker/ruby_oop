c45b80e7fc0b1d1bafb4df8c2b79fe47f8e2c4f7
Charlie Whittaker <cwhittaker@spartaglobal.com>
2015-02-17 15:24:04 +0000

    capitalize first name

--- oop_exercise.rb
+++ oop_exercise.rb
@@ -1,8 +1,18 @@
+require 'pry'
+
 class Person
 
 	def initialize(first_name, surname, dob = 'nil')
+    @first_name = first_name
+    @surname = surname
+    @dob = dob
 
 
+#binding.pry
 	end
 
+attr_accessor :first_name, :surname, :dob
+
 end
+  
+person = Person.new("joe", "bloggs", "1 Jan 1990")  
